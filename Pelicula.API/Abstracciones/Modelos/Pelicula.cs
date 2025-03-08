551fed31b740f82b0ff345d1f30634b0e3272217
JosAry25 <josaraya6183@gmail.com>
2025-03-08 13:29:05 -0600

    Creación de Proyectos

--- Pelicula.API/Abstracciones/Modelos/Pelicula.cs
+++ Pelicula.API/Abstracciones/Modelos/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Abstracciones.Modelos
 {
     public class Pelicula
     {
+        public int ID { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Calificacion { get; set; }
+        public int GeneroID { get; set; }
     }
 }
